71b828367205c3101921e9d9875dccd1d90533e0
Luo wusheng <luowusheng7@gmail.com>
2024-10-21 19:09:14 +0800

    change

--- src/HuffmanTree.h
+++ src/HuffmanTree.h
@@ -1,5 +1,21 @@
 using namespace std;
 
+// 带权节点
+class HuffmanNode {
+public:
+    char data;
+    unsigned int frequency;
+    HuffmanNode* left;
+    HuffmanNode* right;
+
+    // 两个构造函数
+    HuffmanNode(char data, unsigned frequency) 
+        : data(data), frequency(frequency), left(nullptr), right(nullptr) {}
+    HuffmanNode(char data, unsigned frequency, HuffmanNode* left, HuffmanNode* right) 
+        : data(data), frequency(frequency), left(left), right(right) {}
+    
+};
+
 class HuffmanTree {
 public:
     HuffmanNode* root;
